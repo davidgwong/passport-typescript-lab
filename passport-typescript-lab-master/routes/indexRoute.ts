8a4f5179e2530fb3eea6a74ab90fd01ad2920224
David Wong <11532851+davidgwong@users.noreply.github.com>
2023-11-05 01:21:03 -0700

    Added revoke session feature

--- passport-typescript-lab-master/routes/indexRoute.ts
+++ passport-typescript-lab-master/routes/indexRoute.ts
@@ -3,20 +3,10 @@ import session, { SessionData } from "express-session";
 const router = express.Router();
 import { ensureAuthenticated, ensureAdmin } from "../middleware/checkAuth";
 
-router.get("/", (req, res) => {
-  res.send("welcome");
-});
-
-router.get("/dashboard", ensureAuthenticated, (req, res) => {
-  res.render("dashboard", {
-    user: req.user,
-  });
-});
-
-router.get("/admin", ensureAdmin, (req, res) => {
-  let allSessions: { sessionId: string; userId: number }[] = [];
-  const getAllSessions = new Promise<void>((resolve, reject) => {
-    req.sessionStore?.all!(
+let allSessions: { sessionId: string; userId: number }[] = [];
+const getAllSessions = (store: Express.SessionStore) =>
+  new Promise<void>((resolve, reject) => {
+    store?.all!(
       (
         err: any,
         sessions:
@@ -34,6 +24,7 @@ router.get("/admin", ensureAdmin, (req, res) => {
           !Array.isArray(sessions)
         ) {
           const sessionIds = Object.keys(sessions);
+          allSessions = [];
           sessionIds.forEach((sessionId) => {
             allSessions.push({
               sessionId: sessionId,
@@ -46,12 +37,39 @@ router.get("/admin", ensureAdmin, (req, res) => {
     );
   });
 
-  getAllSessions.then(() => {
+router.get("/", (req, res) => {
+  res.send("welcome");
+});
+
+router.get("/dashboard", ensureAuthenticated, (req, res) => {
+  res.render("dashboard", {
+    user: req.user,
+  });
+});
+
+router.get("/admin", ensureAdmin, (req, res) => {
+  getAllSessions(req.sessionStore).then(() => {
     res.render("admin", {
       user: req.user,
       allSessions: allSessions,
     });
-  });  
+  });
+});
+
+router.get("/revoke/:revokeId", ensureAdmin, (req, res) => {
+  const revokeSession = new Promise<void>((resolve, reject) => {
+    req.sessionStore.destroy(req.params.revokeId, (err: any) => {
+      if (err) reject(err);
+      resolve();
+    });
+  });
+  revokeSession
+    .then(() => {
+      return getAllSessions(req.sessionStore);
+    })
+    .then(() => {
+      res.redirect("/admin");
+    });
 });
 
 export default router;
